7b0bcb411e699f58a8f0ac9acbecec09cce01a4c
danilo-sf-dev <danilodeveloperweb@gmail.com>
2025-08-29 05:52:49 -0300

    ajusta responsividade na sessao de navbar

--- src/components/layout/ui/Header.tsx
+++ src/components/layout/ui/Header.tsx
@@ -11,7 +11,7 @@ export const Header: React.FC<HeaderProps> = ({
   return (
     <div className={`md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 ${className}`}>
       <div className="flex items-center justify-between">
-        <div className="flex items-center">
+        <div className="flex items-center min-w-0 flex-1">
           <button
             type="button"
             className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
@@ -20,7 +20,9 @@ export const Header: React.FC<HeaderProps> = ({
             <Menu className="h-6 w-6" />
           </button>
           {title && (
-            <h1 className="ml-2 text-lg font-medium text-gray-900">{title}</h1>
+            <h1 className="ml-2 text-lg font-medium text-gray-900 truncate">
+              {title}
+            </h1>
           )}
         </div>
 
