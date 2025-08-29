2f94f8c39f2e88420e2aa32335f86a82fedf9070
danilo-sf-dev <danilodeveloperweb@gmail.com>
2025-08-29 05:32:04 -0300

    ajusta responsividade na sessao de funcionarios

--- src/components/funcionarios/ui/FuncionariosFilters.tsx
+++ src/components/funcionarios/ui/FuncionariosFilters.tsx
@@ -32,7 +32,7 @@ export const FuncionariosFilters: React.FC<FuncionariosFiltersProps> = ({
 
   return (
     <div className="card space-y-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {/* Busca */}
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
